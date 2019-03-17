06f5f837fff8f3b9702ca62cf0f2154f7b63a7fc
GoncharovVV <goncharov.v.v2017@gmail.com>
2019-03-17 09:24:20 +0200

    Development First Step

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import Swiper from 'swiper';
+import './main.scss';
+// import './index.handlebars';
+// import * as helperModule from './scripts/my-helper-module';
+// console.log(helperModule.greetings);
 var mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 5,
     spaceBetween: 30,
     centeredSlides: true,
     scrollbar: {
         el: '.swiper-scrollbar',
-        hide: true,
+        hide: false,
+        draggable: true
     },
 });
-console.log('hello');
-let addition = (a, b) => a + b;
-console.log(addition(1,2));
+// console.log('hello');
+// let addition = (a, b) => a + b;
+// console.log(addition(1,2));
